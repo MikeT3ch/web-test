f52099be15d5032e179fe14267d5c8dca04e3130
Michael <michaelnunezt@hotmail.com>
2023-11-23 16:51:57 -0500

    Astro Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://MikeT3ch.github.io',
+  base: '/web-test'
 });
